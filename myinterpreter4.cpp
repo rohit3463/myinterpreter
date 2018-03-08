daf8355dcdc3a9454d98f08032ff46c4633e4e85
rohit3463 <rohit.sethi9139666@gmail.com>
2018-03-09 02:15:09 +0530

    added part 4

---
+++ myinterpreter4.cpp
@@ -0,0 +1,191 @@
+#include<iostream>
+#include<cctype>
+#include<string>
+#include<sstream>
+
+#define INTEGER "INTEGER"
+#define DIV "DIV"
+#define EOF "EOF"
+#define MUL "MUL"
+
+using namespace std;
+
+class Token
+{
+	string type;
+	char value;
+public:
+	Token()
+	{
+		this->type = "";
+	}
+	Token(string typeP,char valueP)
+	{
+      this->type = typeP;
+      this->value = valueP;
+	}
+	char get_value()
+	{
+		return this->value;
+	}
+	string get_type()
+	{
+		return this->type; 
+	}
+};
+class Lexer
+{
+	string Text;
+	int Pos;
+	char current_char;
+public:
+	Lexer(string TextP)
+	{
+       this->Text = TextP;
+       this->Pos = 0;
+       this->current_char = this->Text.at(this->Pos); 
+	}
+	void advance()
+	{
+		this->Pos++;
+		if(this->Pos > this->Text.length()-1)
+		{
+			this->current_char = '\0';
+		}
+		else
+		{
+			this->current_char = this->Text.at(this->Pos);
+		}
+	}
+	void skip_whitespace()
+	{ 
+	    while(this->current_char != '\0' && isspace(this->current_char))
+	    {
+	    	this->advance();
+	    }
+
+	}
+	int integer()
+	{
+		string result = "";
+		while(this->current_char != '\0' && isdigit(this->current_char))
+		{
+			result += this->current_char;
+			this->advance();
+		}
+		return stoi(result);
+	}
+	void error()
+	{
+		throw "Error parsing input";
+		return;
+	}
+	Token get_next_token()
+	{
+
+      while(this->current_char != '\0')
+      {
+		if(isspace(this->current_char))
+		{
+			this->skip_whitespace();
+			continue;
+		}
+		
+		if(isdigit(this->current_char))
+		{
+			return Token(INTEGER,this->integer());
+		}
+		if(this->current_char == '*')
+		{
+			this->advance();
+            return Token(MUL,(int)current_char);
+		}
+		if(this->current_char == '/')
+		{    
+			this->advance();
+			return Token(DIV,(int)current_char);
+		}
+	  }
+	}
+  
+};
+class Interpreter
+{
+	Lexer lexer;
+	Token current_token;
+
+public:
+	Interpreter(Lexer arg):lexer(arg)
+	{
+		
+		this->current_token = this->lexer.get_next_token();
+	}
+    void error()
+    {
+    	throw "Syntax error";
+    	return;
+    }	
+
+	void eat(string token_type)
+	{
+		if(this->current_token.get_type() == token_type)
+		{
+			this->current_token = this->lexer.get_next_token();
+		}
+		else
+		{
+			this->error();
+		}
+		return;
+	}
+	char factor()
+	{
+		Token token = this->current_token;
+		this->eat(INTEGER);
+		return token.get_value();
+	}
+	int expr()
+	{
+
+		int result = this->factor();
+		
+		while(this->current_token.get_type() == DIV || this->current_token.get_type() == MUL)
+		{
+           Token token = this->current_token;
+
+           if(token.get_type() == DIV)
+           {
+           	this->eat(DIV);
+           	result /= this->factor();
+           }
+           else if(token.get_type() == MUL)
+           {
+           	this->eat(MUL);
+           	result *= this->factor();
+           }
+		}
+
+		return result;
+
+	}
+};
+
+int main()
+{
+	while(1)
+	{
+		
+			string text;
+			cout<<"Calcgo> ";
+			getline(cin,text,'\n');
+
+			Lexer lexer(text);
+            
+            Interpreter interpreter(lexer);
+
+            int result = interpreter.expr();
+
+            cout<<result<<endl;
+	}
+	return 0;
+}
