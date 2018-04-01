976a8e71e86ac5f2cde9675526d6b2c8fd372845
rohit3463 <rohit.sethi9139666@gmail.com>
2018-04-01 22:54:57 +0530

    main added

---
+++ myinterpreter7/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Interpreter.h"
+
+int main(){
+	std:: string input;
+	int result;
+	while(1){
+		std::cout<<"RCalc> ";
+		getline(std::cin, input, '\n');
+
+		if(input == "exit" || input=="quit") {
+			std::cout<<"Bye!\n";
+			return 0;
+		}
+
+		Lexer lexer(input);
+		Parser parser(lexer);
+		Interpreter interpreter(parser);
+		int result = interpreter.interpret();
+
+		std::cout<<result<<std::endl;
+	}
+
+	return 0;
+}
